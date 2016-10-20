dad8d6af89a677f4f6d32b4f04ac2ea2c5984dc5
Cole Murray <colemurray.cs@gmail.com>
2016-10-20 10:49:50 -0700

    Update rating.sql

--- rating.sql
+++ rating.sql
@@ -2,6 +2,6 @@ CREATE TABLE RATINGS (
 userId varchar(255),
 movieId varchar(255),
 rating float,
-PRIMARY KEY (userId, movieId)
+PRIMARY KEY (userId, movieId),
 FOREIGN KEY (movieId) REFERENCES MOVIES(id)
 );
